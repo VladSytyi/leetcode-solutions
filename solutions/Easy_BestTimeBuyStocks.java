cf9e375e86d52b9ae65c94a1f2238b360a0cc739
Vladislav Sytyi <valdis3gs@gmail.com>
2024-03-20 12:20:14 +0100

    feat(description): added description to the tasks

--- solutions/Easy_BestTimeBuyStocks.java
+++ solutions/Easy_BestTimeBuyStocks.java
@@ -14,6 +14,8 @@ class Easy_BestTimeBuyStocks {
      Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
          Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
     *
+    * Topics: Array, Dynamic Programming
+    * Patterns: Sliding Window,
     * */
     public static void main(String[] args) {
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
--- solutions/Easy_TwoSum.java
+++ solutions/Easy_TwoSum.java
@@ -5,12 +5,17 @@ import java.util.Map;
 public class Easy_TwoSum {
 
     /*
+
+    1. Two Sum
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
     You can return the answer in any order.
 
     Input: nums = [2,7,11,15], target = 9
     Output: [0,1]
+
+    Topics: Arrays, HashTable
+    Patterns: Two Pointers, Memoization
      */
 
     public static void main(String[] args) {
